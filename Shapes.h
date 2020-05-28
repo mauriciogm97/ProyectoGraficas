57c2c34642f8d62ec071e20cc5f6453546cb9efe
mauriciogm97 <mauriciogm97@hotmail.com>
2020-05-28 01:38:06 -0500

    Refactor

--- Shapes.h
+++ Shapes.h
@@ -6,57 +6,8 @@
 #include <cstdlib>
 #include <algorithm> 
 
-#include <iostream>
-
-
 void drawCube(GLfloat centerX, GLfloat width, GLfloat centerY, GLfloat height, GLfloat centerZ, GLfloat depth, GLfloat color[]);
 void drawTrapezoid(GLfloat centerX, GLfloat lowerWidth, GLfloat upperWidth, GLfloat centerY, GLfloat height, GLfloat centerZ, GLfloat depth, GLfloat color[]);
-void drawSphere(GLfloat centerX, GLfloat centerY, GLfloat centerZ, GLfloat diameter);
-
-void drawSphere(GLfloat centerX, GLfloat centerY, GLfloat centerZ, GLfloat diameter) {
-
-    GLfloat color[3888*3];
-    for (int x = 0; x < 3888 * 3; x += 3) {
-        color[x] = 0;
-        color[x+1] = 255;
-        color[x+2] = 0;
-    }
-
-    GLfloat radius = diameter / 2.0;
-    GLfloat verticesX[1296];
-    GLfloat verticesY[1296];
-    GLfloat verticesZ[1296];
-    int vCont = 0;
-    for (int x = 1; x <= 360; x += 10) {
-        for (int y = 1; y <= 360; y += 10) {
-            verticesX[vCont] = centerX + (radius * cosf(x)) * cosf(y);
-            verticesY[vCont] = centerY + (radius * cosf(x)) * sinf(y);
-            verticesZ[vCont++] = centerZ + radius * sinf(x);
-        }
-    }
-
-    GLfloat vertices[3888];
-    vCont = 0;
-    for (int x = 1; x < 1296; x++) {
-        vertices[vCont++] = verticesX[x];
-        vertices[vCont++] = verticesY[x];
-        vertices[vCont++] = verticesZ[x];
-    }
-    
-   
-    //std::sort(vertices, vertices + 3888);
-
-    //glPolygonMode( GL_FRONT_AND_BACK, GL_LINE ); // Se coment� esta linea para quitar poder hacer solidos a los objetos
-    glEnable(GL_DEPTH_TEST); //Agregar la proyecci�n de profundidad
-    glDepthMask(GL_TRUE);//Agregar la proyecci�n de profundidad
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-    glVertexPointer(3, GL_FLOAT, 0, vertices);
-    glColorPointer(3, GL_FLOAT, 0, color); //Buffer de color
-    glDrawArrays(GL_QUADS, 0, 3888);
-    glDisableClientState(GL_VERTEX_ARRAY);
-    glDisableClientState(GL_COLOR_ARRAY);
-}
 
 void drawCube(GLfloat centerX, GLfloat width, GLfloat centerY, GLfloat height, GLfloat centerZ, GLfloat depth, GLfloat color[]) {
     drawTrapezoid(centerX, width, width, centerY, height, centerZ, depth, color);
